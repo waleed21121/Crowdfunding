7eb2c967ff26fbad244fa6693cb8e74c4b0822f5
waleed21121 <wa581509@gmail.com>
2025-08-25 13:34:31 +0300

    Created verify jwt middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,10 +2,12 @@ import errorHandler from "./error.middleware";
 import validationMiddleware from "./validation.middleware";
 import wrapErrorMiddleware from "./wrapError.middleware";
 import requestLogger from "./requestLogger";
+import verifyJwtMiddleware from "./verifyJwt.middleware";
 
 export {
     errorHandler,
     validationMiddleware,
     wrapErrorMiddleware,
-    requestLogger
+    requestLogger,
+    verifyJwtMiddleware
 }
