6f1102fc8f50ac3ad651d802bc6017955b29e637
waleed21121 <wa581509@gmail.com>
2025-08-27 13:02:20 +0300

    Created pledge controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,10 +2,12 @@ import campaignController from "./campaign.controller";
 import userController from "./user.controller";
 import campaignMilestoneController from "./campaignMilestone.controller";
 import rewardTierController from "./rewardTier.controller";
+import pledgeController from "./pledge.controller";
 
 export {
     campaignController,
     userController,
     campaignMilestoneController,
-    rewardTierController
+    rewardTierController,
+    pledgeController
 }
---
+++ src/controllers/pledge.controller.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Response } from "express";
+import { pledgeService } from "../services";
+import { IPledgeResponse } from "../dtos";
+import { TIdValidator } from "../validators";
+import { StatusCodes } from "http-status-codes";
+
+const findOne: TIdValidator = async (req, res: Response<IPledgeResponse>, next: NextFunction) => {
+    const campaignMilestone = await pledgeService.findOne(req.params.id)
+    res.status(StatusCodes.OK).send({
+        success: true,
+        message: 'Pledge milestone found successfully',
+        data: campaignMilestone,
+        error: null
+    });
+}
+
+const pledgeController = {
+    findOne,
+}
+
+export default pledgeController
